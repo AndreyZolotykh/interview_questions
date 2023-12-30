5e122a9d7a0615db19ec3fb20a4f716034da91a4
AndreyZolotykh <59248314+AndreyZolotykh@users.noreply.github.com>
2023-12-30 12:27:26 +0000

    Add second example

---
+++ main2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <list>
+#include <vector>
+
+template <class T>
+void impl(T& data) {
+    data.push_back(0);
+    auto iterator = data.begin();
+
+    for (size_t i = 1u; i < 100u; ++i) {
+        data.push_back(i);
+    }
+
+    std::cout << "First = " << *iterator << std::endl;
+    iterator = data.begin();
+    std::cout << "First = " << *iterator << std::endl;
+}
+
+int main() {
+    std::list<int> data_1;
+    impl(data_1);
+
+    std::vector<int> data_2;
+    impl(data_2);
+}
